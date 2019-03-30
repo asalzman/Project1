f715c5271bf502f8a0521ea468f1e95306d30018
asalzman <48000062+asalzman@users.noreply.github.com>
2019-03-30 16:01:37 -0400

    Add files via upload

--- allie proj.py
+++ allie proj.py
@@ -2,34 +2,35 @@ import csv
 import pandas as pd
 import requests
 import json
+import unicodecsv as csv
 
-df = pd.read_csv(r'/Users/michelleglewis/Downloads/ira_tweets_csv_hashed.csv', low_memory = False)
+df = pd.read_csv(r'/Users/michelleglewis/Downloads/ira_tweets_csv_hashed.csv', low_memory = False, usecols = ["tweet_text"])
 
 dictionary = {}
 
-for i in df:
-    tweet = df[13][i]
-    data = tweet.json()
-    words = tweet.split()
+for i in df['tweet_text']:
+    words = i.split()
+    count = count + 1
     for x in words:
         if x not in dictionary:
             dictionary[x] = 1
         else:
-            dictionary[x] = dictionary[x] + 1    
+            dictionary[x] = dictionary[x] + 1
+            
+listofwords = [(list(dictionary.keys())[i], list(dictionary.values())[i]) for i in range(len(dictionary.keys()))]
 
-listofwords = list((dictionary.keys(), dictionary.values()))
 sortedlist = sorted(listofwords, key=lambda tup: tup[1], reverse=True)
 word1 = sortedlist[0]
 word2 = sortedlist[1]
-word3 = sortedList[2]
+word3 = sortedlist[2]
 word4 = sortedlist[3]
 word5 = sortedlist[4]
 
-print(word1[0] + ":" + word1[1])
-print(word2[0] + ":" + word2[1])
-print(word3[0] + ":" + word3[1])
-print(word4[0] + ":" + word4[1])
-print(word5[0] + ":" + word5[1])
+print(word1[0] + ":" + str(word1[1]))
+print(word2[0] + ":" + str(word2[1]))
+print(word3[0] + ":" + str(word3[1]))
+print(word4[0] + ":" + str(word4[1]))
+print(word5[0] + ":" + str(word5[1]))
 
 
 
