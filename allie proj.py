14e0da082c31f3d2e4ac6b921606af5b0c15d25e
asalzman <48000062+asalzman@users.noreply.github.com>
2019-04-06 16:01:15 -0400

    Add files via upload

--- allie proj.py
+++ allie proj.py
@@ -1,40 +1,35 @@
-import csv
-import pandas as pd
+#import csv
 import requests
 import json
 import unicodecsv as csv
 
-df = pd.read_csv(r'/Users/michelleglewis/Downloads/ira_tweets_csv_hashed.csv', low_memory = False, usecols = ["tweet_text"])
+#f = open("/Users/michelleglewis/Downloads/ira_tweets_csv_hashed.csv", "rb")
+userinput = input('Enter name of file')
+f = open(userinput, "rb")
+df = csv.reader(f, encoding = 'utf-8')
 
 dictionary = {}
 
-for i in df['tweet_text']:
-    words = i.split()
-    count = count + 1
+count = 0
+_ = next(df)
+for i in df:
+    words = i[12].split()
     for x in words:
         if x not in dictionary:
             dictionary[x] = 1
         else:
             dictionary[x] = dictionary[x] + 1
-            
-listofwords = [(list(dictionary.keys())[i], list(dictionary.values())[i]) for i in range(len(dictionary.keys()))]
+    if count%100000 == 0:
+        print (count)
+    count = count + 1
+
+listofword = [(x, dictionary[x]) for x in dictionary.keys()]
+# not sure if x is the right thing to use in ^
+#listofwords = [(list(dictionary.keys())[i], list(dictionary.values())[i]) for i in range(len(dictionary.keys()))]
 
 sortedlist = sorted(listofwords, key=lambda tup: tup[1], reverse=True)
-word1 = sortedlist[0]
-word2 = sortedlist[1]
-word3 = sortedlist[2]
-word4 = sortedlist[3]
-word5 = sortedlist[4]
 
-print(word1[0] + ":" + str(word1[1]))
-print(word2[0] + ":" + str(word2[1]))
-print(word3[0] + ":" + str(word3[1]))
-print(word4[0] + ":" + str(word4[1]))
-print(word5[0] + ":" + str(word5[1]))
-
-
-
-
-
-
-        
+for i in range(5):
+    word = sortedlist[i]
+    print(word[0] + str(word[1])
+          
